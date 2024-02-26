ac0bf9d383d289d8f59b1140b1a838f735ace0bf
bigboifloppa1 <101159417+bigboifloppa1@users.noreply.github.com>
2024-02-26 23:26:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 var a = navigator.geolocation;
 var btn = document.querySelector('.btn');
+var div = document.querySelector('.div');
 
 function getLocation() {
   if (navigator.geolocation) {
     a.getCurrentPosition((position) => {
-      document.body.innerHTML = `<h1> latitude: ${position.coords.latitude}  <br/> longitude: ${position.coords.longitude} </h1> <br/> <p class=\"googleit\"> google it ;) ${position.coords.latitude},  ${position.coords.longitude} </p>`;
+      document.body.innerHTML = `<h1> latitude: ${position.coords.latitude}  <br/> longitude: ${position.coords.longitude} </h1> <br/> <div class = \"div\"> <p class=\"googleit\"> google it ;) ${position.coords.latitude},  ${position.coords.longitude} </p> </div>`;
     });
   }
 }
 
 function handleOrientationChange() {
   if (window.orientation !== 0) {
+    //landscape
+    //.btn
     btn.style.width = "10%";
     btn.style.height = "10%";
   } else {
+    //portrait
+    //.btn
     btn.style.width = "20%";
     btn.style.height = "5%";
+    //.googleit
+    googleit.style.left = "-1000px";
   }
 }
 
@@ -23,5 +30,5 @@ setInterval(() => {
   handleOrientationChange();
   
   window.addEventListener("orientationchange", handleOrientationChange);
-},500);
+},10);
 
