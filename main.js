d6675d3d0c0dfb21ff325a4f6fb297fbb8a8d69a
bigboifloppa1 <101159417+bigboifloppa1@users.noreply.github.com>
2024-02-26 20:54:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var a = navigator.geolocation;
 function getLocation() {
   if (navigator.geolocation) {
     a.getCurrentPosition((position) => {
-      document.write = (`<h1> latitude: ${position.coords.latitude}  <br/> longitude: ${position.coords.longitude} </h1>`);
+      document.body.innerHTML = `<h1> latitude: ${position.coords.latitude}  <br/> longitude: ${position.coords.longitude} </h1> <br/> <p class=\"googleit\"> google it ;) ${position.coords.latitude},  ${position.coords.longitude} </p>`;
     });
   }
 }
